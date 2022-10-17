866fee413343d19b5a499a771f2d44dc6c1dc21b
Joseph Park <parkjoseph54@gmail.com>
2022-10-17 18:40:01 +0000

    completed operators and expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 20;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 100;
+var payment = 90;
+var change = bill - payment;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 95;
+var midterm = 91;
+var final = 98;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Joseph';
+var lastName = 'Park';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 3.4;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 10;
+var isSparta = headCount === 300;
+console.log('value of isSparata:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
