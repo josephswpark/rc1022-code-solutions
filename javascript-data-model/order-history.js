76ce75534aaf763b2c372d55cd12031753561d35
Joseph Park <parkjoseph54@gmail.com>
2022-10-18 18:59:50 +0000

    updated data model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,55 +1,78 @@
 var orderHistory = [
   {
     orderNumber: '114-3941689-8772232',
-    bookName: 'JavaScript for Impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    bookImage: 'images/images1.png',
     ordered: 'August 4, 2020',
-    total: '$34.00',
-    bookRetailPrice: '$31.55',
-    shipTo: 'JS Masher',
+    total: 34.00,
+    receiver: 'JS Masher',
     delivered: 'Delivered Aug 8, 2020',
-    deliveryLocation: 'Your package was delivered.It was handed directly to a resident.',
-    returnBy: 'Return window Closed on Sep 7, 2020'
+    items: [
+      {
+        type: 'book',
+        title: 'JavaScript for Impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        image: 'images/jv.png',
+        returnBy: 'Sep 7, 2020',
+        retailPrice: 31.55,
+        deliveredTo: 'Your package was delivered.It was handed directly to a resident.'
+      }
+    ]
   },
   {
     orderNumber: '113-9984268-1280257',
-    bookName: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    bookImage: 'images/images2.png',
     ordered: 'July 19, 2020',
-    total: '$44.53',
-    bookRetailPrice: '$41.33',
-    shipTo: 'JS Masher',
+    total: 44.53,
+    receiver: 'JS Masher',
     delivered: 'Delivered Jul 20, 2020',
-    deliveryLocation: 'Your package was delivered.It was handed directly to a resident.',
-    returnBy: 'Return window Closed on Aug 19, 2020'
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        retailPrice: 41.33,
+        image: 'images/images2.png',
+        deliveredTo: 'Your package was delivered.It was handed directly to a resident.',
+        returnBy: 'Aug 19, 2020'
+      }
+    ]
   },
   {
     orderNumber: '114-2875557-9059409',
-    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter ',
-    itemImage: 'images/images3.png',
     ordered: 'July 4, 2020',
-    total: '$17.22',
-    itemRetailPrice: '$15.98',
-    shipTo: 'JS Masher',
+    total: 17.22,
+    receiver: 'JS Masher',
     delivered: 'Delivered Jul 7, 2020',
-    deliveryLocation: 'Your package was delivered.It was handed directly to a resident.',
-    returnBy: 'Return window Closed on Aug 5, 2020'
+    items: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter ',
+        image: 'images/images3.png',
+        retailPrice: 15.98,
+        deliveredTo: 'Your package was delivered.It was handed directly to a resident.',
+        returnBy: 'Aug 5, 2020'
+      }
+    ]
   },
   {
     orderNumber: '113-2883177-2648248',
-    itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-    itemImage: 'images/images4.png',
-    bookName: 'The Art of Sql',
-    author: 'Faroult, Stephane',
-    bookImage: 'images/images3.png',
     ordered: 'July 3, 2020',
-    total: '$138.93',
-    itemRetailPrice: '$94.95',
-    bookRetailPrice: '$33.99',
-    shipTo: 'JS Masher',
+    total: 138.93,
+    receiver: 'JS Masher',
     delivered: 'Delivered Jul 5, 2020',
-    returnBy: 'Return window Closed on Aug 4, 2020'
+    items: [
+      {
+        type: 'controller',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        retailPrice: 94.95,
+        image: 'images/controller2.png',
+        returnBy: 'Aug 4, 2020'
+      },
+      {
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        retailPrice: 33.99,
+        bookImage: 'images/book3.png',
+        returnBy: 'Aug 4, 2020'
+      }
+    ]
   }
 ];
