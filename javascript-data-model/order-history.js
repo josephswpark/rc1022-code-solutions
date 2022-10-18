73805481e7aa3a6e93577e0f6ad5144ad76b0cff
Joseph Park <parkjoseph54@gmail.com>
2022-10-18 17:19:42 +0000

    completed data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,55 @@
+var orderHistory = [
+  {
+    ordered: 'August 4, 2020',
+    total: '$34.00',
+    ship: 'JS Masher',
+    delivered: 'Delivered Aug 8, 2020',
+    deliveryLocation: 'Your package was delivered.It was handed directly to a resident.',
+    orderNumber: '114-3941689-8772232',
+    title: 'JavaScript for Impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnBy: 'Return window Closed on Sep 7, 2020',
+    retailPrice: '$31.55',
+    imgage: 'images/images1.png'
+  },
+  {
+    ordered: 'July 19, 2020',
+    total: '$44.53',
+    ship: 'JS Masher',
+    delivered: 'Delivered Jul 20, 2020',
+    deliveryLocation: 'Your package was delivered.It was handed directly to a resident.',
+    orderNumber: '113-9984268-1280257',
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnBy: 'Return window Closed on Aug 19, 2020',
+    retailPrice: '$41.33',
+    image: 'images/images2.png'
+  },
+  {
+    ordered: 'July 4, 2020',
+    total: '$17.22',
+    ship: 'JS Masher',
+    delivered: 'Delivered Jul 7, 2020',
+    deliveryLocation: 'Your package was delivered.It was handed directly to a resident.',
+    orderNumber: '114-2875557-9059409',
+    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter ',
+    returnBy: 'Return window Closed on Aug 5, 2020',
+    retailPrice: '$15.98',
+    imgage: 'images/images3.png'
+  },
+  {
+    ordered: 'July 3, 2020',
+    total: '$138.93',
+    ship: 'JS Masher',
+    delivered: 'Delivered Jul 5, 2020',
+    orderNumber: '113-2883177-2648248',
+    itemName1: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    itemName2: 'The Art of Sql',
+    bookTitle: 'Faroult, Stephane',
+    returnBy: 'Return window Closed on Aug 4, 2020',
+    gameCubePrice: '$94.95',
+    bookPrice: '$33.99',
+    bookImgage: 'images/images3.png',
+    gameImage: 'images/images4.png'
+  }
+];
