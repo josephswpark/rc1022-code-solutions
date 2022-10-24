db0767e77c31858a72da912069f5f36de03dda40
Joseph Park <parkjoseph54@gmail.com>
2022-10-24 20:35:18 +0000

    updated dom events

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,26 +1,27 @@
 function handleClick(event) {
+  console.log('button clicked');
   console.log(event);
   console.log(event.target);
 }
-console.log('button clicked');
 
 var lo = document.querySelector('.click-button');
 lo.addEventListener('click', handleClick);
 
 function handleMouseover(event) {
+  console.log('button hovered');
   console.log(event);
   console.log(event.target);
+
 }
-console.log('button hovered');
 
 var hover = document.querySelector('.hover-button');
 hover.addEventListener('mouseover', handleMouseover);
 
 function handleDoubleClick(event) {
+  console.log('button double-clicked');
   console.log(event);
   console.log(event.target);
 }
-console.log('button double-clicked');
 
 var doubled = document.querySelector('.double-click-button');
 doubled.addEventListener('dblclick', handleDoubleClick);
