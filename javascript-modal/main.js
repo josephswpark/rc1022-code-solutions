1d7434975303ad851ea1754c91a2be3fedea9ac3
Joseph Park <parkjoseph54@gmail.com>
2022-10-26 20:56:18 +0000

    in progress

---
+++ javascript-modal/main.js
@@ -0,0 +1,11 @@
+var modal = document.querySelector('.modal');
+var trigger = document.querySelector('.trigger');
+var noButton = document.querySelector('.no-button');
+
+trigger.addEventListener('click', function () {
+  modal.classList.toggle('show-modal');
+});
+
+noButton.addEventListener('click', function () {
+  modal.classList.remove('show-modal');
+});
