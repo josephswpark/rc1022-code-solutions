4a890efcf8a31b9a4b822ca997041d003a0427f1
Joseph Park <parkjoseph54@gmail.com>
2022-10-27 16:51:11 +0000

    updated js-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,12 +1,26 @@
+/* eslint-disable no-unused-vars */
 var modal = document.querySelector('.modal');
 var trigger = document.querySelector('.trigger');
 var noButton = document.querySelector('.no-button');
 
-trigger.addEventListener('click', function () {
+function onClick() {
+  modal.classList.toggle('show-modal');
+}
+
+function popUpClick(event) {
+  if (event.target === modal) {
+    modal.classList.remove('show-modal');
+  }
+}
+
+trigger.addEventListener('click', onClick);
+noButton.addEventListener('click', onClick);
+
+/* trigger.addEventListener('click', function () {
   modal.classList.toggle('show-modal');
 
 });
 
 noButton.addEventListener('click', function () {
   modal.classList.remove('show-modal');
-});
+}); */
