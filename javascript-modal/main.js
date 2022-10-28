a50fe800d8c81adc6bc62bb749ea9a2fc05b8db0
Joseph Park <parkjoseph54@gmail.com>
2022-10-28 15:59:33 +0000

    updated modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -7,20 +7,5 @@ function onClick() {
   modal.classList.toggle('show-modal');
 }
 
-function popUpClick(event) {
-  if (event.target === modal) {
-    modal.classList.remove('show-modal');
-  }
-}
-
 trigger.addEventListener('click', onClick);
 noButton.addEventListener('click', onClick);
-
-/* trigger.addEventListener('click', function () {
-  modal.classList.toggle('show-modal');
-
-});
-
-noButton.addEventListener('click', function () {
-  modal.classList.remove('show-modal');
-}); */
