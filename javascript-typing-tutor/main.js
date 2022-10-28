04cd2fc221b024bbb452844ffb12714f53b219ae
Joseph Park <parkjoseph54@gmail.com>
2022-10-28 15:56:28 +0000

    completed

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,25 +3,20 @@ var currentIndex = 0;
 document.addEventListener('keydown', typingTest);
 
 function typingTest(event) {
-  // if we are at the end of the test, alert and reload page
   if (currentIndex === charNodes.length) {
     alert('DONE! Restarting Test....');
     window.location.reload();
   }
-  // if not backspace, validate character
   if (event.keyCode !== 8) {
     checkCharacterIsValid(event.key, currentIndex);
   }
-  // we want to update index when going forward or back
   if (event.key === charNodes[currentIndex].innerHTML || event.keyCode === 8) {
     updateCurrentIndex(event.keyCode);
   }
-  // if we are NOT at the end of the test, underline the next character
   if (currentIndex !== charNodes.length) {
     underlineNewCharacter(currentIndex);
   }
 }
-// function to increment/decrement current index
 function updateCurrentIndex(keyCode) {
   resetUnderline(currentIndex);
   if (keyCode === 8 && currentIndex > 0) {
@@ -31,7 +26,6 @@ function updateCurrentIndex(keyCode) {
     currentIndex += 1;
   }
 }
-// function to check if keydown matches the current node
 function checkCharacterIsValid(currentChar, charIndex) {
   var currentNode = charNodes[charIndex];
   if (currentChar === currentNode.innerHTML) {
@@ -40,17 +34,16 @@ function checkCharacterIsValid(currentChar, charIndex) {
     currentNode.className = 'incorrect';
   }
 }
-// function takes in node index, resets classname to empty string
 function resetCharacter(nodeIndex) {
   charNodes[nodeIndex].className = '';
 }
-// function takes in current index and adds class = current
+
 function underlineNewCharacter(currentIndex) {
   if (!charNodes[currentIndex].classList.contains('current')) {
     charNodes[currentIndex].classList.add('current');
   }
 }
-// function takes current index and removes class=current
+
 function resetUnderline(currentIndex) {
   charNodes[currentIndex].classList.remove('current');
 }
