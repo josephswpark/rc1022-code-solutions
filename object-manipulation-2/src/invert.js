45000d694bd97cda305b9f9b87466d1908af3361
Joseph Park <parkjoseph54@gmail.com>
2022-11-05 23:38:34 +0000

    in progress

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,11 @@
 /* exported invert */
+
+function invert(source) {
+  var invertedObject = {};
+  for (var key in source) {
+    invertedObject[source[key]] = key;
+  }
+  return invertedObject;
+}
+
+// pickedObject[keys[i]] = source[keys[i]];
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,20 @@
 /* exported omit */
+/*
+input: an object & keys which are properties
+create a new object
+if (keys )
+
+out: a new object with keys that was not matched
+*/
+
+// function omit(source, keys) {
+//   var notPresentObject = {};
+//   var sourceObjectKeys = Object.keys(source); // --> ['foo', 'bar', 'baz']
+//   for (var i = 0; i < Object.length + 1; i++) {
+//     if (sourceObjectKeys[i] === keys[i]) {
+//       sourceObjectKeys.shift();
+//     }
+//   }
+//   console.log(sourceObjectKeys);
+//   return notPresentObject;
+// }
