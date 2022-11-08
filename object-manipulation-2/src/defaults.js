08b259121b89fc014f0f9a3ed42e346fb33bc00e
Joseph Park <parkjoseph54@gmail.com>
2022-11-08 00:26:53 +0000

    in progress

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,17 +1,20 @@
 /* exported defaults */
 /*
-input: target --> any object & source --> any object
-bring the source into the target object
-using the assign method of the Object.
-merge target into source and then into target to keep the original property and value
+What are our inputs: target that is an object & source is an object
+check for the key in source
+if the source & target key overlap, apply the
+
+target will be the destination because the key:value of target has to stay while source gets removed
 return: does not return anything but it modifies target
 */
 
-function defaults(target, source) { // obj1 is destination, obj2 is source
+function defaults(target, source) {
   for (var key in source) {
     // eslint-disable-next-line no-prototype-builtins
     if (source.hasOwnProperty(key) && !target.hasOwnProperty(key)) {
       target[key] = source[key];
+      // console.log('target[key]:', target[key]);
+      // console.log(source[key]);
     }
   }
   return target;
