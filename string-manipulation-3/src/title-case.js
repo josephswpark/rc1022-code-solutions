3c369f568de7bc09eb41c8f87601d1b95c5d852e
Joseph Park <parkjoseph54@gmail.com>
2022-11-17 05:31:59 +0000

    in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,13 @@
 /* exported titleCase */
+// input: a string that is a book title
+//
+
+// output: original title with AP title case style applied
+
+// function titleCase(title) {
+//   for (var i = 0; i < title.length; i++) {
+//   }
+//   if (title.toLowerCase() === 'javascript') {
+//     word = 'JavaScript';
+//   }
+// }
