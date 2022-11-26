c87065fecf761468129b60af50696f4b16ef2a42
Joseph Park <parkjoseph54@gmail.com>
2022-11-26 21:06:47 +0000

    completed array-manipulation-3

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,37 @@
 /* exported union */
+// union(first, second)
+// Parameters
+// first - Any JavaScript Array
+// second - Any JavaScript Array
+// Return Value
+// A new Array containing unique values from both first and second in the order they appear.
+
+//   Examples
+// union([2, 1], [2, 3]);
+// // -> [2, 1, 3]
+// union(['html', 'css', 'javascript'], ['php', 'css', 'sql']);
+// // -> ["html", "css", "javascript", "php", "sql"]
+// union(
+//   ['a', 'link', 'to', 'the', 'past'],
+//   ['the', 'adventure', 'of', 'link']
+// )
+// // -> ["a", "link", "to", "the", "past", "adventure", "of"]
+
+// input: an array
+//
+// return: a new array containing values from both first and second in the order they appear
+
+function union(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (newArray.indexOf(first[i]) === -1) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var a = 0; a < second.length; a++) {
+    if (newArray.indexOf(second[a]) === -1) {
+      newArray.push(second[a]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,19 @@
 /* exported unique */
+// input: any JS array
+// create a newArray empty storage array
+// create a for loop to iterate through the array input
+// if the newArray at each index is present at each index of the array,-- use .indexOf()
+// .indexOf(searchElement, startIndex)
+// then push it into the newArray
+// if not, you skip if it doesn't meet the condition
+// Return: a new Array containing only the unique elements of array, in the order they first appeared
+
+function unique(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (newArray.indexOf(array[i]) === -1) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
