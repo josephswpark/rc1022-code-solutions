02cfc0d3a19a31054ce62774f610942e38d6c217
Joseph Park <parkjoseph54@gmail.com>
2022-11-29 23:36:35 +0000

    completed node-process-argv

---
+++ node-process-argv/echo.js
@@ -0,0 +1,8 @@
+// console.log('hello');
+// console.log('command line arguments!');
+// console.log('hello, computer!');
+// console.log('no YOU are a computer.');
+// console.log('STOP THAT!');
+// console.log('STOP THAT!', 'hello');
+
+console.log(process.argv[2]);
