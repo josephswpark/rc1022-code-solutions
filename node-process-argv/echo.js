58c5254180a17182b82311ceac6810f5706f4780
Joseph Park <parkjoseph54@gmail.com>
2022-11-29 23:49:06 +0000

    updated echo.js

--- node-process-argv/echo.js
+++ node-process-argv/echo.js
@@ -1,8 +1,1 @@
-// console.log('hello');
-// console.log('command line arguments!');
-// console.log('hello, computer!');
-// console.log('no YOU are a computer.');
-// console.log('STOP THAT!');
-// console.log('STOP THAT!', 'hello');
-
 console.log(process.argv[2]);
