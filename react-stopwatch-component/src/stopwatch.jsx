585c003d6cf5998b93e69d227d22a62b4cc391ed
Joseph Park <parkjoseph54@gmail.com>
2022-12-16 21:39:52 +0000

    completed stopwatch

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { time: 0, play: false, interval: null };
+    this.reset = this.reset.bind(this);
+    this.start = this.start.bind(this);
+  }
 
+  start() {
+    if (this.state.play === false) {
+      this.setState({ play: true });
+      this.setState({
+        interval: setInterval(() => {
+          this.setState({ time: this.state.time + 1 });
+        }, 1000)
+      });
+    } else {
+      this.setState({ play: false });
+      clearInterval(this.state.interval);
+    }
+  }
+
+  reset() {
+
+    this.setState({ time: 0 });
+  }
+
+  render() {
+    const icon = this.state.play ? 'fa-pause' : 'fa-play';
+    const time = this.state.time;
+    return (
+      <div className="container">
+        <div className="background">
+          <div className="timer" onClick={ this.reset }>{ time }</div>
+        </div>
+        <i className={'fa-solid icon ' + icon } onClick={ this.start }/>
+      </div>
+    );
   }
 }
