0c8e5af501dbb7c11a2f6ead1b68eff6ab67dbd3
Joseph Park <parkjoseph54@gmail.com>
2022-12-19 21:43:05 +0000

    in progress

---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Carousel from './carousel';
+
+// const img = [
+//   { id: 0, image: '../images/001.png' },
+//   { id: 1, image: '../images/004.png' },
+//   { id: 2, image: '../images/007.png' },
+//   { id: 2, image: '../images/025.png' },
+//   { id: 2, image: '../images/039.png' }
+// ];
