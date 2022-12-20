c253f0103ccc93b3cd60f2f40b81f33501fbe6fc
Joseph Park <parkjoseph54@gmail.com>
2022-12-20 20:16:28 +0000

    completed carousel

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -1,11 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Carousel from './carousel';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from './carousel';
 
-// const img = [
-//   { id: 0, image: '../images/001.png' },
-//   { id: 1, image: '../images/004.png' },
-//   { id: 2, image: '../images/007.png' },
-//   { id: 2, image: '../images/025.png' },
-//   { id: 2, image: '../images/039.png' }
-// ];
+const img = [
+  { id: 0, image: '../images/001.png' },
+  { id: 1, image: '../images/004.png' },
+  { id: 2, image: '../images/007.png' },
+  { id: 2, image: '../images/025.png' },
+  { id: 2, image: '../images/039.png' }
+];
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<Carousel images={img}/>);
