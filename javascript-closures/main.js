cfcd5c3de1a3315259d35128ff6e347f0c278908
Joseph Park <parkjoseph54@gmail.com>
2023-01-11 23:57:39 +0000

    completed closures

---
+++ javascript-closures/main.js
@@ -0,0 +1,11 @@
+function graduate(credential) {
+  return fullName => {
+    return `${fullName} ${credential}`;
+  };
+}
+
+const medicalSchool = graduate('M.D');
+const lawSchool = graduate('Esq.');
+
+console.log(medicalSchool('Dan Abramov'));
+console.log(lawSchool('Ryan Florence'));
